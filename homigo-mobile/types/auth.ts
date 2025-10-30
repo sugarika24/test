866e8a26ecca5a5612d88256700503506949dd4c
sugarika24 <sugarikam@gmai.com>
2025-10-30 22:18:56 +0545

    Update: homigo-mobile/types/auth.ts

--- homigo-mobile/types/auth.ts
+++ homigo-mobile/types/auth.ts
@@ -24,13 +24,31 @@ export interface UploadFile {
   type: string;
 }
 
+export interface SendRegisterEmailOtpPayload {
+  email: string;
+  full_name?: string;
+}
+
+export interface VerifyRegisterEmailOtpPayload {
+  email: string;
+  otp_code: string;
+}
+
+// export interface SendRegisterOtpPayload {
+//   phone_number: string;
+// }
+
+// export interface VerifyRegisterOtpPayload {
+//   phone_number: string;
+//   otp_code: string;
+// }
 
 export interface RegisterPayload {
   full_name: string;
   email: string;
   password: string;
   confirm_password: string;
-  phone_number?: string;
+  phone_number: string;
   role: "USER" | "HELPER";
   id_document_type?: "CITIZENSHIP" | "NATIONAL_ID" | "PASSPORT" | "DRIVING_LICENSE";
   id_document_number?: string;
